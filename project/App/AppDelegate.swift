cc6ddcef532cf8ca9dcc7b928af1fc8302163bba
ChoiAnYong <dksdyd78@naver.com>
2024-03-30 18:14:56 +0900

    feat: gitignore

---
+++ project/App/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  project
+//
+//  Created by 최안용 on 3/30/24.
+//
+
+import Foundation
---
+++ project/Model/Path/PathModel.swift
@@ -0,0 +1,12 @@
+//
+//  PathModel.swift
+//  project
+//
+//  Created by 최안용 on 3/30/24.
+//
+
+import Foundation
+
+final class PathModel: ObservableObject {
+    
+}
